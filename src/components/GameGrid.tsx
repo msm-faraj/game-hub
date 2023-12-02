5896e79cc4ccc8ad6bc0507f4b024b1a153a80f4
msm-faraj <farajzademeysam@gmail.com>
2023-12-02 18:03:36 +0100

    Get Optimize Images

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 2, md: 3, lg: 4, xl: 5 }}
         spacing={5}
         padding={"10px"}
       >
